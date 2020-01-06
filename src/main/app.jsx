9e51603af7e00dc012f082e1c12f94ca6f7d6558
Weslley Borges <weslleynfs@gmail.com>
2020-01-06 18:10:36 -0400

    Progresso curso react

---
+++ src/main/app.jsx
@@ -0,0 +1,10 @@
+import  '../common/templates/dependencies'
+import React from 'react'
+
+import Header from '../common/templates/header'
+
+export default props => (
+  <div className="wrapper">
+    <Header></Header>  
+  </div>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: 'src/loader.jsx',
+  entry: './src/index.jsx',
   output: {
     path: __dirname + '/public',
     filename: './app.js'
@@ -38,7 +38,7 @@ module.exports = {
       }
     }, {
       test: /\.css$/,
-      loader: ExtractTextPlugin.extract('styler-loader', 'css-loader')
+      loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
     }, {
       test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
       loader: 'file'
